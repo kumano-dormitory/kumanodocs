8a7bddaad0ace9140d5f63334da79ddc0c404ec9
Y.Sangenya <longinus.eva@gmail.com>
2015-10-25 00:41:50 +0900

    バックアップ
    daily backupを、sqldiffを使って、差分バックアップにした。
    月に二回、ブロック会議の直後である、7日と22日にDropboxにバックアップを取るようにした。

--- deploy/backup.sh
+++ deploy/backup.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
 cd /srv/kumanodocs/
-cp db.sqlite3 deploy/backup/kumanodocs-database-backup-$(date +"%Y-%m-%d").sqlite3
+SQL_BACKUP_PATH=deploy/backup
+if test "07" = $(date +"%d") -o "22" = $(date +"%d"); then
+    SQL_SEED=${SQL_BACKUP_PATH}/kumanodocs-database-backup-seed-$(date +"%Y-%m-%d").sqlite3
+    cp db.sqlite3 ${SQL_SEED}
+    gzip -c ${SQL_SEED} > ${SQL_SEED}.gz
+    ./deploy/upload_dropbox.py ${SQL_SEED}.gz
+    rm ${SQL_SEED}.gz
+else
+    SOURCE_BACKUP=${SQL_BACKUP_PATH}/kumanodocs-database-backup-seed-$(date +"%Y-%m").sqlite3
+    if [ ! -e ${SOURCE_BACKUP}]; then
+        cp db.sqlite3 ${SOURCE_BACKUP}
+        rm ${SQL_BACKUP_PATH}/kumanodocs-database-backup-diff*
+    fi
+    DIFF_BACKUP=${SQL_BACKUP_PATH}/kumanodocs-database-backup-diff-$(date +"%Y-%m-%d").sqlite3
+    sqldiff ${SOURCE_BACKUP} db.sqlite3 > ${DIFF_BACKUP}
+fi
