ef766cc1d1bd4a588dca332db351b4c7f1a0606e
Hayato Hashimoto <hayato.hashimoto@gmail.com>
2016-03-04 02:56:30 +0900

    add issuetype to admin page

--- document_system/admin.py
+++ document_system/admin.py
@@ -4,5 +4,6 @@ from document_system.models import Meeting, Issue, Note, Block, IssueType, Table
 # Register your models here.
 admin.site.register(Meeting)
 admin.site.register(Issue)
+admin.site.register(IssueType)
 admin.site.register(Note)
 admin.site.register(Table)
