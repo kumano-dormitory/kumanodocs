dedcbe229ce6fbcee4a9c86652a3db8447a92138
Hayato Hashimoto <hayato.hashimoto@gmail.com>
2016-03-04 03:40:33 +0900

    issuetypeを/admin/で変更できないように

--- document_system/admin.py
+++ document_system/admin.py
@@ -4,6 +4,5 @@ from document_system.models import Meeting, Issue, Note, Block, IssueType, Table
 # Register your models here.
 admin.site.register(Meeting)
 admin.site.register(Issue)
-admin.site.register(IssueType)
 admin.site.register(Note)
 admin.site.register(Table)
