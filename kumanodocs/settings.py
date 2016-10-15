c74a4053bcc7cca224af900b74c8d25335384a7e
Yusuke Sangenya <sangenya@sancha-mac.local>
2016-10-15 19:52:46 +0900

    /var/dbをdockerのdata volumeにする布石

--- kumanodocs/settings.py
+++ kumanodocs/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'kumanodocs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/var/db/db.sqlite3',
     }
 }
 
